26a81a4c4fa08b3a8f674e5ad1d9c3828f109073
unknown <pandachka874@gmail.com>
2024-03-03 14:05:21 +0300

    updated

--- my_script.js
+++ my_script.js
@@ -1,51 +1,14 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+$(".nav-link").click(function (e) {
+  $('.nav-link').removeClass('active')
+  $('.tab-pane').removeClass('active').addClass('fade')
 
-// const title = document.getElementById("mainTitle");
-
-// console.log(title);
-
-// window.console
-
-// window.document
-
-// window.$
-
-// console.log({
-//   window,
-//   jQuery,
-// });
-
-// document
-
-// console.log(typeof $);
-// $("mainTitle");
-
-// console.log($("#mainTitle"));
-// $("p").each(function (idx, el) {
-//   console.log(idx, el);
-
-//   $(el).addClass("new");
-// });
-
-// $("#handler").click(function (e) {
-//   console.log(e);
-//   //   $(".wrap").show();
-//   //   setTimeout(() => $(".wrap").hide(), 3000);
-//   $(".wrap").slideToggle();
-// });
-// e = event
-$(".open-modal").click(function (e) {
   const eventTarget = e.target;
-  eventTarget.dataset.target = eventTarget.dataset.target; //прикол
   const targetSelector = $(eventTarget).data("target");
-
-  //   const targetElement = $(targetSelector);
-  $(".modal-wrapper").addClass("active");
-  $(targetSelector).addClass("active");
+  $(targetSelector).removeClass('fade').addClass("active");
+  $(this).addClass('active')
 });
-$(".modal-window .close, .modal-wrapper").click(function (event) {
-  if (this === event.target) {
-    $(".modal-wrapper").removeClass("active");
-    $(".modal-window").removeClass("active");
-  }
-});
+// $(".").click(function (event) {
+//   $(".modal-wrapper").removeClass("active");
+//   $(".modal-window").removeClass("active");
+// });
