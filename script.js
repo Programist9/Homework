e534957484d1f9ff7271d8f4a113da69b9ce09ed
panda <pandachka874@gmail.com>
2024-03-23 12:31:28 +0200

    updated

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// const hello_world = "Hello, World!";
-// const number_test = 1;
-// console.log(hello_world);
-// console.log(number_test);
-// console.log(hello_world, number_test);
-//
 const dropdownItems = document.querySelectorAll(".dropdown");
 dropdownItems.forEach((item) => {
   item.addEventListener("click", function (e) {
